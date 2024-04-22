5a947413b80df6fef00226dddcbd0ae36e0b980c
Kijai <40791699+kijai@users.noreply.github.com>
2024-04-22 17:35:47 +0300

    Add AnimationGPT

--- nodes/mgpt_nodes.py
+++ nodes/mgpt_nodes.py
@@ -8,11 +8,20 @@ from model_management import get_torch_device
 import inspect
 import motiondiff_modules
 from pathlib import Path
+from comfy.utils import load_torch_file
 
 class mgpt_model_loader:
     @classmethod
     def INPUT_TYPES(s):
         return {"required": {
+            "model": (
+                [   
+                    "MotionGPT",
+                    "AnimationGPT",
+                ],
+                {
+                "default": "MotionGPT-base"
+                    }),
         },
         }
 
@@ -21,24 +30,27 @@ class mgpt_model_loader:
     FUNCTION = "loadmodel"
     CATEGORY = "MotionDiff/mGPT"
 
-    def loadmodel(self):
+    def loadmodel(self, model):
         script_directory = os.path.dirname(inspect.getabsfile(motiondiff_modules))
         config_path = os.path.join(script_directory, "mGPT/configs/inference.yaml")
         cfg = OmegaConf.load(config_path)
-        if not hasattr(self, 'model') or self.model == None:
-            base_bath = os.path.join(script_directory, 'mGPT','checkpoints','MotionGPT-base')
-            model_path = os.path.join(script_directory, base_bath,'motiongpt_s3_h3d.tar')
-            if os.path.exists(model_path):
-                state_dict = torch.load(model_path, map_location="cpu")["state_dict"]
-            else:
-                #https://huggingface.co/OpenMotionLab/MotionGPT-base/tree/main to ComfyUI/custom_nodes/ComfyUI-MotionDiff/mGPT/checkpoints
+
+        if not hasattr(self, "model") or self.model == None or model != self.model_name:
+            self.model_name = f"{model}_fp16.safetensors"
+            base_bath = os.path.join(script_directory, "mGPT","checkpoints")
+            model_path = os.path.join(script_directory, base_bath, self.model_name)
+
+            if not os.path.exists(model_path):
+                #https://huggingface.co/Kijai/AnimationGPT_pruned/ to ComfyUI/custom_nodes/ComfyUI-MotionDiff/motiondiff_modules/mGPT/checkpoints
+                print(f"Downloading model to {model_path}")
                 from huggingface_hub import snapshot_download
-                snapshot_download(repo_id="OpenMotionLab/MotionGPT-base",
-                                    local_dir=Path(script_directory).parent/"ckpts", local_dir_use_symlinks=False, allow_patterns=['motiongpt_s3_h3d.tar'])  
-                state_dict = torch.load(model_path, map_location="cpu")["state_dict"]
+                snapshot_download(repo_id="Kijai/AnimationGPT_pruned",
+                                    local_dir=model_path, local_dir_use_symlinks=False, allow_patterns=[f"*{self.model_name}*"])  
+            
+            state_dict = load_torch_file(model_path)
 
             data_config = OmegaConf.to_container(cfg.DATASET, resolve=True)
-            data_config['params'] = {'cfg': cfg, 'phase': 'test'}
+            data_config["params"] = {"cfg": cfg, "phase": "test"}
             datamodule = instantiate_from_config(data_config)
             self.model = build_model(cfg, datamodule)
             
@@ -53,7 +65,7 @@ class mgpt_t2m:
             "mgpt_model": ("MGPTMODEL",),
             "motion_length": ("INT", {"default": 196, "min": 1, "max": 196, "step": 1}),
             "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
-            "text": ("STRING", {"multiline": True,"default": 'make the person jump and turn around'}),
+            "text": ("STRING", {"multiline": True,"default": "make the person jump and turn around"}),
             }
         }
 
@@ -68,7 +80,7 @@ class mgpt_t2m:
 
         prompt = mgpt_model.lm.placeholder_fulfill(text, motion_length, "", "")
         batch = {
-            "length": [motion_length], #I don't know what this is supposed to do if anything? Lenght seems to be determined by the prompt up to the max of 196
+            "length": [motion_length], #I don"t know what this is supposed to do if anything? Lenght seems to be determined by the prompt up to the max of 196
             "text": [prompt],
         }
         mgpt_model.to(device)
